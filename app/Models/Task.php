f9b5c3c442e6ff55899e561efe34be4217a7e880
Sovon Saha <saha.sovon89@gmail.com>
2021-09-21 17:41:35 +0600

    Expiry time scope removed

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,23 +2,12 @@
 
 namespace App\Models;
 
-use App\Scopes\ExpiryScope;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
     use HasFactory;
-
-    /**
-     * The "booted" method of the model.
-     *
-     * @return void
-     */
-    protected static function booted()
-    {
-        static::addGlobalScope(new ExpiryScope);
-    }
 
     /**
      * The attributes that are mass assignable.
